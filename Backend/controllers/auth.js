3b313210f5ddb5882e0ac3f6ac4789c21aa548a8
housssem1999 <mhoussem789@gmail.com>
2021-07-19 02:46:32 +0100

    Added login method

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -21,3 +21,36 @@ const User = require("../models/User");
         err:err.message,});
       }
     };
+
+    // @desc    Login user
+exports.login = async (req, res, next) => {
+    const { email, password } = req.body;
+  
+    // Check if email and password is provided
+    if (!email || !password) {
+        res.status(400).json({success:false,error:"Please provide email and password"})
+    }
+  
+    try {
+      // Check that user exists by email
+      const user = await User.findOne({ email }).select("+password");
+  
+      if (!user) {
+        res.status(404).json({success:false,error:"Invalid credentials" })
+      }
+  
+      // Check that password match
+      const isMatch = await user.matchPassword(password);
+  
+      if (!isMatch) {
+        res.status(404).json({success:false,error:"Invalid credentials" })
+      }res.status(200).json({
+          success:true,
+          token:"tr34f3443fcc",
+      });
+    } catch (err) {
+        res.status(500).json({
+            sucess :false,
+            err:err.message,});
+          }
+  };
